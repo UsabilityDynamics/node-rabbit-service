da6e092d0352da915475f2abcce0e3f90ca295d1
Andy Potanin <andy.potanin@usabilitydynamics.com>
2013-11-04 16:17:40 -0500

    Adding vendor distributions.

--- bin/bash/install.sh
+++ bin/bash/install.sh
@@ -1,3 +1,16 @@
-# Install Module
+# Install RabbitMQ Module
 #
-#
+
+# Link Binaries
+ln -f vendor/rabbitmq-3.2.0/sbin/rabbitmq-defaults bin/rabbitmq-defaults
+ln -f vendor/rabbitmq-3.2.0/sbin/rabbitmq-env bin/rabbitmq-env
+ln -f vendor/rabbitmq-3.2.0/sbin/rabbitmq-server bin/rabbitmq-server
+ln -f vendor/rabbitmq-3.2.0/sbin/rabbitmq-plugins bin/rabbitmq-plugins
+ln -f vendor/rabbitmq-3.2.0/sbin/rabbitmqctl bin/rabbitmqctl
+
+# Link Configuration
+ln -f static/etc/enabled_plugins vendor/rabbitmq-3.2.0/etc/rabbitmq/enabled_plugins
+ln -f static/etc/enabled_plugins vendor/rabbitmq-3.2.0-mac/etc/rabbitmq/enabled_plugins
+
+# mkdir /etc/rabbitmq
+# ln -f static/etc/rabbitmq.config /etc/rabbitmq/rabbitmq-env.conf
