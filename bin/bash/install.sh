40d2ec00ea87088141fc98c7a8726871f5bec49b
Andy Potanin <andy.potanin@usabilitydynamics.com>
2013-11-06 15:57:22 -0500

    WIP

--- bin/bash/install.sh
+++ bin/bash/install.sh
@@ -1,19 +1,15 @@
 # Install RabbitMQ Module
 #
 
-# Link Configuration Files
-# ln -f static/etc/rabbitmq.config vendor/rabbitmq-3.2.0/etc/rabbitmq/rabbitmq.config
-# ln -f static/etc/enabled_plugins vendor/rabbitmq-3.2.0/etc/rabbitmq/enabled_plugins
-# ln -f static/etc/rabbitmq.config vendor/rabbitmq-3.2.0-mac/etc/rabbitmq/rabbitmq.config
-# ln -f static/etc/enabled_plugins vendor/rabbitmq-3.2.0-mac/etc/rabbitmq/enabled_plugins
-
-# Create Directories
-if [ ! -d ".dynamic" ]; then
-  rm -rf .dynamic
-  mkdir .dynamic
-fi
-
 # Flush Dynamic Directories
+rm -rf .dynamic && mkdir .dynamic
 rm -rf .dynamic/pid && mkdir .dynamic/pid
+rm -rf .dynamic/lib && mkdir .dynamic/lib
+rm -rf .dynamic/log && mkdir .dynamic/log
 rm -rf .dynamic/cache && mkdir .dynamic/cache
-rm -rf .dynamic/logs && mkdir .dynamic/logs
+
+# Link Configuration Files
+ln -f static/etc/rabbitmq.config vendor/rabbitmq-3.2.0/etc/rabbitmq/rabbitmq.config
+ln -f static/etc/enabled_plugins vendor/rabbitmq-3.2.0/etc/rabbitmq/enabled_plugins
+ln -f static/etc/rabbitmq.config vendor/rabbitmq-3.2.0-mac/etc/rabbitmq/rabbitmq.config
+ln -f static/etc/enabled_plugins vendor/rabbitmq-3.2.0-mac/etc/rabbitmq/enabled_plugins
--- bin/bash/update.sh
+++ bin/bash/update.sh
@@ -1,15 +1,15 @@
 # Update RabbitMQ Module
 #
 
-
 # Create Directories
 if [ ! -d ".dynamic" ]; mkdir .dynamic; fi
 if [ ! -d ".dynamic/cache" ]; mkdir .dynamic/cache; fi
 if [ ! -d ".dynamic/pid" ]; mkdir .dynamic/pid; fi
-if [ ! -d ".dynamic/logs" ]; mkdir .dynamic/logs; fi
+if [ ! -d ".dynamic/lib" ]; mkdir .dynamic/lib; fi
+if [ ! -d ".dynamic/log" ]; mkdir .dynamic/log; fi
 
 # Link Configuration Files
-# ln -f static/etc/rabbitmq.config vendor/rabbitmq-3.2.0/etc/rabbitmq/rabbitmq.config
-# ln -f static/etc/enabled_plugins vendor/rabbitmq-3.2.0/etc/rabbitmq/enabled_plugins
-# ln -f static/etc/rabbitmq.config vendor/rabbitmq-3.2.0-mac/etc/rabbitmq/rabbitmq.config
-# ln -f static/etc/enabled_plugins vendor/rabbitmq-3.2.0-mac/etc/rabbitmq/enabled_plugins
+ln -f static/etc/rabbitmq.config vendor/rabbitmq-3.2.0/etc/rabbitmq/rabbitmq.config
+ln -f static/etc/enabled_plugins vendor/rabbitmq-3.2.0/etc/rabbitmq/enabled_plugins
+ln -f static/etc/rabbitmq.config vendor/rabbitmq-3.2.0-mac/etc/rabbitmq/rabbitmq.config
+ln -f static/etc/enabled_plugins vendor/rabbitmq-3.2.0-mac/etc/rabbitmq/enabled_plugins
