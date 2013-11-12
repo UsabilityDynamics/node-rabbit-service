eec1caf7826eaa05702f692bf62de1ace99abf79
Andy Potanin <andy.potanin@usabilitydynamics.com>
2013-11-12 12:06:14 -0500

    WIP

--- bin/bash/install.sh
+++ bin/bash/install.sh
@@ -8,7 +8,7 @@ rm -rf .dynamic/log && mkdir .dynamic/log
 rm -rf .dynamic/cache && mkdir .dynamic/cache
 
 # Link Configuration Files
-ln -f static/etc/rabbitmq.config vendor/rabbitmq-3.2.0/etc/rabbitmq/rabbitmq.config
-ln -f static/etc/enabled_plugins vendor/rabbitmq-3.2.0/etc/rabbitmq/enabled_plugins
-ln -f static/etc/rabbitmq.config vendor/rabbitmq-3.2.0-mac/etc/rabbitmq/rabbitmq.config
-ln -f static/etc/enabled_plugins vendor/rabbitmq-3.2.0-mac/etc/rabbitmq/enabled_plugins
+# ln -f static/etc/rabbitmq.config vendor/rabbitmq-3.2.0/etc/rabbitmq/rabbitmq.config
+# ln -f static/etc/enabled_plugins vendor/rabbitmq-3.2.0/etc/rabbitmq/enabled_plugins
+# ln -f static/etc/rabbitmq.config vendor/rabbitmq-3.2.0-mac/etc/rabbitmq/rabbitmq.config
+# ln -f static/etc/enabled_plugins vendor/rabbitmq-3.2.0-mac/etc/rabbitmq/enabled_plugins
--- bin/bash/update.sh
+++ bin/bash/update.sh
@@ -8,7 +8,7 @@ if [ ! -d ".dynamic/pid" ]; mkdir .dynamic/pid; fi
 if [ ! -d ".dynamic/log" ]; mkdir .dynamic/log; fi
 
 # Link Configuration Files
-ln -f static/etc/rabbitmq.config vendor/rabbitmq-3.2.0/etc/rabbitmq/rabbitmq.config
-ln -f static/etc/enabled_plugins vendor/rabbitmq-3.2.0/etc/rabbitmq/enabled_plugins
-ln -f static/etc/rabbitmq.config vendor/rabbitmq-3.2.0-mac/etc/rabbitmq/rabbitmq.config
-ln -f static/etc/enabled_plugins vendor/rabbitmq-3.2.0-mac/etc/rabbitmq/enabled_plugins
+# ln -f static/etc/rabbitmq.config vendor/rabbitmq-3.2.0/etc/rabbitmq/rabbitmq.config
+# ln -f static/etc/enabled_plugins vendor/rabbitmq-3.2.0/etc/rabbitmq/enabled_plugins
+# ln -f static/etc/rabbitmq.config vendor/rabbitmq-3.2.0-mac/etc/rabbitmq/rabbitmq.config
+# ln -f static/etc/enabled_plugins vendor/rabbitmq-3.2.0-mac/etc/rabbitmq/enabled_plugins
